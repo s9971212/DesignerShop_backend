f4a45433d01569ca4632e3e91946b851d0b622db
Ivan <s9971212@yahoo.com.tw>
2024-07-15 19:14:49 +0800

    first commit

---
+++ src/main/java/com/designershop/DesignershopWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.designershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignershopWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignershopWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designershop/DesignershopWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignershopWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
