bac8114df73b327d7083d73e1ea73b5aed68994b
Ivan <s9971212@yahoo.com.tw>
2024-12-28 12:35:40 +0800

    [project][feat] 設置CORS允許跨域請求

---
+++ src/main/java/com/designershop/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.designershop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Ivan Wang
+ * @version 1.0
+ * @date 2024/12/22
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST", "GET", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
